522519d7f7692fd31c3d431b36d590308647fb7f
Anoop <sripaanoop@gmail.com>
2021-09-08 10:31:32 +0530

    Take a photo

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,13 +1,20 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_detect_poc/auth.dart';
+import 'package:photo_detect_poc/take_picture_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key, required this.auth}) : super(key: key);
   final AuthBase auth;
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   Future<void> _signOut() async {
     try {
-      await auth.signOut();
+      await widget.auth.signOut();
     } catch (e) {
       // ignore: avoid_print
       print(e.toString());
@@ -31,6 +38,31 @@ class HomePage extends StatelessWidget {
             onPressed: _signOut,
           ),
         ],
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton.icon(
+              onPressed: () async {
+                // Obtain a list of the available cameras on the device.
+                final cameras = await availableCameras();
+                // Get a specific camera from the list of available cameras.
+                final firstCamera = cameras.first;
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => TakePictureScreen(
+                      camera: firstCamera,
+                    ),
+                  ),
+                );
+              },
+              icon: const Icon(Icons.camera_alt_outlined),
+              label: const Text('Take a picture'),
+            ),
+          ],
+        ),
       ),
     );
   }
